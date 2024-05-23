90b6e6f7387e6d566f43a293a16f803b02f01c97
Arvellian <arvellianputra@gmail.com>
2024-05-24 03:06:59 +0700

    docs: add berita_acara_gambar table

---
+++ database/migrations/2024_02_09_115607_create_berita_acara_gambars_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('berita_acara_gambar', function (Blueprint $table) {
+            $table->bigIncrements('id_gambar');
+            $table->bigInteger('id_berita_acara')->unsigned()->index();
+            $table->string('path');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('berita_acara_gambar');
+    }
+};
